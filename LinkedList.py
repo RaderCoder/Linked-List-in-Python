38674c8d3ac9c6ec9d7e0cb194b38b29b611ec6a
Radee Al-Mahmood <radee.mahmood@northsouth.edu>
2023-04-01 01:53:10 +0600

    Added search and print methods

--- LinkedList.py
+++ LinkedList.py
@@ -1,8 +1,35 @@
 class Node:
-    def __init__(self, value, next: None):
+    def __init__(self, value, next = None):
         self.value = value
         self.next = next
 
 class LinkedList:
-    def __init__(self, head: None):
-        self.head = head
+    def __init__(self, head = None):
+        self.head = Node(head)
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def find_value(self,value):
+        temp = self.head
+        found = False
+        while temp:
+            if temp.value == value:
+                found = True
+                break
+            else:
+                temp = temp.next
+        
+        return found
+
+    def __repr__(self):
+        repstr = ""
+        temp = self.head
+        
+        while temp:
+            repstr = repstr + str(temp.value) + " "
+            temp = temp.next
+
+        return repstr
---
+++ main.py
@@ -0,0 +1,13 @@
+# For Testing
+import LinkedList
+
+if __name__ == "__main__":
+    New_list = LinkedList.LinkedList(6)
+    New_list.push(5)
+    New_list.push(3)
+
+    print(New_list)
+
+    print(New_list.find_value(5))
+
+    print(New_list.find_value(4))
